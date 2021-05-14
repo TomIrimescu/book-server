362a53bd3a07b78772aae94678e6a155a256717f
Tom Irimescu <tom@irimescu.com>
2021-05-14 16:37:44 -0400

    Refactor typeorm & ts config, add express middleware & postgres db

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
-import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { ApolloServer } from "apollo-server";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import "reflect-metadata";
 
 import { BookResolver } from "./resolvers/BookResolver";
+
+require("dotenv").config();
 
 async function main() {
   await createConnection();
   const schema = await buildSchema({ resolvers: [BookResolver] });
   const server = new ApolloServer({ schema });
-  await server.listen(5000);
-  console.log("Server has started!");
+
+  const app = express();
+  const router = express.Router();
+  server.applyMiddleware({
+    app,
+    cors: true,
+  });
+
+  app.use(express.json());
+
+  app.use(router);
+  router.get("/", (req, res, next) => {
+    res.send("Hello from Express Router");
+  });
+
+  await app.listen({ port: process.env.SERVER_PORT }, () => {
+    console.log(
+      `GraphQl endpoint found at http://localhost:${process.env.SERVER_PORT}${server.graphqlPath}`
+    );
+  });
 }
 
 main();
